f7ea4648070959f8a93c5577df6e4a508d308bcb
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-07-13 02:21:03 -0700

    rest of the routes -AP

--- MerchantMachine/routes/routepass.go
+++ MerchantMachine/routes/routepass.go
@@ -99,13 +99,13 @@ func RouteDigest(digest *chi.Mux, firebaseAuth *firebase.App) *chi.Mux{
 	digest.Get("/category/primes", rAdmin.ReturnAllPrimeCategories)
 	digest.Get("/category/subs", rAdmin.ReturnAllSubCategories)
 	digest.Get("/category/finals", rAdmin.ReturnAllFinalCategories)
-	// digest.Patch("/products/{ProductID}",rAdmin.EditProduct)
-	// digest.Patch("/variation/{VariationID}",rAdmin.EditVariation)
-	// digest.Post("/variation/{VariationID}/attribute",rAdmin.AddAttribute)
-	// digest.Patch("/variation/{VariationID}/attribute/{AttributeName}",rAdmin.UpdateAttribute)
-	// digest.Delete("/variation/{VariationID}/attribute/{AttributeName}",rAdmin.DeleteAttribute)
-	// digest.Post("/admin/{UserID}", rAdmin.UserToAdmin)
+	digest.Patch("/products/{ProductID}",rAdmin.EditProduct)
+	digest.Patch("/variation/{VariationID}",rAdmin.EditVariation)
+	digest.Post("/variation/{VariationID}/attribute",rAdmin.AddAttribute)
+	digest.Patch("/variation/{VariationID}/attribute/{AttributeName}",rAdmin.UpdateAttribute)
+	digest.Delete("/variation/{VariationID}/attribute/{AttributeName}",rAdmin.DeleteAttribute)
+	digest.Post("/admin/{UserID}", rAdmin.UserToAdmin)
 
-	// digest.Get("/tables",rAdmin.GetAllTables)
+	digest.Get("/tables",rAdmin.GetAllTables)
 	return digest
 }
