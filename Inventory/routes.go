64331c54c3fe3a314b6184e0c92baf4d25be0295
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2025-06-22 06:13:52 -0700

    routes for inventory retrieval -AP

--- Inventory/routes.go
+++ Inventory/routes.go
@@ -6,7 +6,14 @@ import (
 )
 
 func routerDigest(router *chi.Mux){
-	router.Get("/inventory-locations",retrival.InventoryLocation)
+	router.Get("/inventory-locations",retrival.InventoryLocationAll)
+	router.Get("/inventory-locations/{id}",retrival.InventoryLocation)
+	router.Get("/inventory-locations/",retrival.HandleSearch)
+	router.Get("/inventory-locations-products",retrival.AllProductLocations)
+	router.Get("/inventory-locations-products/{location}",retrival.ProductsInSingularLocations)
+	router.Get("/inventory-locations-transfers",retrival.AllTransfers)
+	router.Get("/inventory-locations-transfers/{id}",retrival.TransfersByID)
+	router.Get("/inventory-locations-transfers/",retrival.HandleSearch)
 }
 
 
