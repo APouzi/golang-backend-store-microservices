937441ef2c36416f933c6fc07e53d0f4d57e4bac
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2025-06-22 06:14:04 -0700

    changed up port number -AP

--- Inventory/main.go
+++ Inventory/main.go
@@ -23,8 +23,8 @@ func main() {
 
 	
 	routerDigest(r)
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	log.Println("Starting server on :8004")
+	if err := http.ListenAndServe(":8004", r); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
 }
