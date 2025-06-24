37ee0b9351c6a06418a29809e32079311687aff0
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2025-06-24 01:00:25 -0700

    GetProductInventoriesFromParameter - AP

--- DBLayer/inventory/inventory-endpoints.go
+++ DBLayer/inventory/inventory-endpoints.go
@@ -64,9 +64,63 @@ func (routes *InventoryRoutesTray) GetAllLocations(w http.ResponseWriter, r *htt
 }
 
 
-func (routes *InventoryRoutesTray) GetAllProductInventoriesFromLocation(w http.ResponseWriter, r *http.Request) {
-	locationID := chi.URLParam(r, "product-variation-id")
-	if locationID == "" {
+func (routes *InventoryRoutesTray) GetProductInventoriesFromParameter(w http.ResponseWriter, r *http.Request) {
+	locationID := r.URL.Query().Get("location-id")
+	productID := r.URL.Query().Get("product_id")
+	var query string
+	var queried_var string
+	if locationID != "" {
+		query = "SELECT inventory_id, quantity, product_id, location_id, description FROM tblInventoryProductDetail WHERE location_id = ?"
+		queried_var = locationID
+	}else if productID != "" {
+		query = "SELECT inventory_id, quantity, product_id, location_id, description FROM tblInventoryProductDetail WHERE product_id = ?"
+		queried_var = productID
+	}else{
+		http.Error(w,"No Parameter Found",http.StatusBadRequest)
+	}
+	
+	tx, err := routes.DB.Begin()
+	if err != nil {
+		http.Error(w, "Failed to start DB transaction", http.StatusInternalServerError)
+		log.Println("Begin transaction error:", err)
+		return
+	}
+	defer tx.Rollback()
+
+	rows, err := tx.Query(query, queried_var)
+	if err != nil {
+		http.Error(w, "Failed to fetch locations", http.StatusInternalServerError)
+		log.Println("Query error:", err)
+		return
+	}
+	defer rows.Close()
+
+	var locations []InventoryProductDetail
+	for rows.Next() {
+		var loc InventoryProductDetail
+		err := rows.Scan(&loc.InventoryID, &loc.Quantity,&loc.ProductID,&loc.LocationID,&loc.Description)
+		if err != nil {
+			http.Error(w, "Failed to parse result", http.StatusInternalServerError)
+			log.Println("Row scan error:", err)
+			return
+		}
+		locations = append(locations, loc)
+	}
+
+	if err := tx.Commit(); err != nil {
+		http.Error(w, "Failed to commit transaction", http.StatusInternalServerError)
+		log.Println("Commit error:", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(locations); err != nil {
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		log.Println("JSON encode error:", err)
+		return
+	}
+}
+
 		http.Error(w, "Missing product-variation-id parameter", http.StatusBadRequest)
 		return
 	}
