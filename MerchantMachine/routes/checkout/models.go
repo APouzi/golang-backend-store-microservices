9a8e6484d75453b0e82fe539cff3be58c47cca0f
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2025-07-30 23:00:16 -0700

    added productsize model since we added a third layer -AP

--- MerchantMachine/routes/checkout/models.go
+++ MerchantMachine/routes/checkout/models.go
@@ -61,8 +61,21 @@ type InventoryProductDetail struct {
 
 type FrontendRequest struct {
 	Items []struct {
-		Product_ID int64 `json:"Product_ID"`
+		Size_ID int64 `json:"size_id"`
 		Quantity   int64  `json:"quantity"`
 	} `json:"items"`
 
+}
+
+type ProductSize struct {
+    SizeID         *int64       `db:"Size_ID" json:"size_id"`
+    SizeName       *string     `db:"Size_Name" json:"size_name"`
+    SizeDescription *string    `db:"Size_Description,omitempty" json:"size_description,omitempty"`
+    VariationID    *int64        `db:"Variation_ID" json:"variation_id"`
+    VariationPrice *float64    `db:"Variation_Price" json:"variation_price"`
+    SKU            *string    `db:"SKU,omitempty" json:"sku,omitempty"`
+    UPC            *string    `db:"UPC,omitempty" json:"upc,omitempty"`
+         *string    `db:"PRIMARY_IMAGE,omitempty" json:"primary_image,omitempty"`
+    DateCreated    *time.Time  `db:"Date_Created" json:"date_created"`
+    ModifiedDate   *time.Time `db:"Modified_Date,omitempty" json:"modified_date,omitempty"`
 }
