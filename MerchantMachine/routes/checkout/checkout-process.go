2e8d8284f69a99ec9f39c6c26788e7a566934ac1
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2025-07-30 23:00:48 -0700

    getproductsize updates -AP

--- MerchantMachine/routes/checkout/checkout-process.go
+++ MerchantMachine/routes/checkout/checkout-process.go
@@ -47,8 +47,27 @@ func GetProductVariationByID(prodID string, ProdJSON *[]ProductResponse, w http.
 	}
 }
 
+func GetProductSizeByID(productSizeID string, ProdJSON *ProductSize, w http.ResponseWriter) {
+	url := "http://dblayer:8080/product-size/" + productSizeID
+	resp, err := http.Get(url)
+	jd := json.NewDecoder(resp.Body)
+	jd.Decode(ProdJSON)
+	if err != nil {
+		helpers.ErrorJSON(w, err, 400)
+		fmt.Println("failed to decode response:", err)
+	}
+	// if len(*ProdJSON) == 0 {
+	// 	helpers.ErrorJSON(w, errors.New("there was no response"), 404)
+	// 	return
+	// }
+	if err != nil {
+		helpers.ErrorJSON(w, err, 400)
+		fmt.Println("failed to pull product:", err)
+	}
+}
+
 func GetProductInventoryDetailByID(prodID string, InvProdJSON *[]InventoryProductDetail, w http.ResponseWriter) {
-	url := "http://dblayer:8080/inventory/inventory-product-details/?product_id=" + prodID
+	url := "http://dblayer:8080/inventory/inventory-product-details/?product_size_id=" + prodID
 	resp, err := http.Get(url)
 	jd := json.NewDecoder(resp.Body)
 	jd.Decode(InvProdJSON)
